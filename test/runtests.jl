09b01a0b7a28f25195d6d368a312f2898dbd8b39
Richard Dennis <richard.dennis@glasgow.ac.uk>
2019-06-15 22:24:08 +0100

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,27 @@ using PiecewiseLinearApprox
 using Test
 
 @testset "PiecewiseLinearApprox.jl" begin
-    # Write your own tests here.
+
+  function foo(x)
+
+    y = log.(x[1])*log.(x[2])'
+    return y
+
+  end
+
+  x1 = piecewise_linear_nodes(11,[3.0,1.0])
+  x2 = piecewise_linear_nodes(21,[2.5,0.5])
+
+  x = (x1,x2)
+  y = foo(x)
+
+  point = [2.1,0.7]
+
+  y_hat = piecewise_linear_evaluate(y,x,point)
+
+  y_actual = log(point[1])*log(point[2])
+  diff = (y_actual - y_hat)
+  println(diff)
+  @test abs(diff) <= 1e-3
+  
 end

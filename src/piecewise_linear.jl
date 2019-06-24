8bf72b0a117a8961903aa82a496c58b2c1198913
Richard Dennis <richard.dennis@glasgow.ac.uk>
2019-06-24 22:29:06 +0100

    Update piecewise_linear.jl
    Interpolation uses only the essential grid points.

--- src/piecewise_linear.jl
+++ src/piecewise_linear.jl
@@ -12,6 +12,100 @@ function piecewise_linear_nodes(n::S,domain = [1.0,-1.0]) where {S <: Integer}
 
 end
 
+function bracket_nodes(x::Array{T,1},point::T) where {T <: AbstractFloat}
+
+    if point <= x[1]
+        return (1,2)
+    elseif point >= x[end]
+        return (length(x) - 1, length(x))
+    else
+        return (sum(point .> x), sum(point .> x) + 1)
+    end
+
+end
+
+function bracket_nodes(x::Union{NTuple{N,Array{T,1}},Array{Array{T,1},1}},point::Array{T,1}) where {T <: AbstractFloat, N}
+
+    bracketing_nodes = zeros(Int64,2,length(x))
+    for i = 1:length(point)
+        bracketing_nodes[:,i] .= bracket_nodes(x[i],point[i])
+    end
+    return bracketing_nodes
+end
+
+function piecewise_linear_weight(x::Array{T,1},point::T) where {T <: AbstractFloat}
+
+    bracketing_nodes = bracket_nodes(x,point)
+    weight = (point-x[bracketing_nodes[1]])/(x[bracketing_nodes[2]]-x[bracketing_nodes[1]])
+
+    return weight
+
+end
+
+function piecewise_linear_weights(x::Union{NTuple{N,Array{T,1}},Array{Array{T,1},1}},point::Array{T,1}) where {T <: AbstractFloat, N}
+
+    weights = Array{T}(undef,length(point))
+    for i = 1:length(point)
+        weights[i] = piecewise_linear_weight(x[i],point[i])
+    end
+
+    return weights
+
+end
+
+function select_bracketing_nodes(bounds::Array{S,2} where {S <: Integer})
+
+  d = size(bounds,2)
+
+  number_relevant_grid_points = 2^d
+  bracketing_grid_points      = Array{Int64}(undef,number_relevant_grid_points,d)
+
+  for i = 1:d
+    bracketing_grid_points[:,i] .= repeat(repeat(bounds[:,i],inner=[2^(d-i),1]),2^(i-1))[:]
+  end
+
+  return bracketing_grid_points
+
+end
+
+function select_relevant_data(y::AbstractArray{T,N},bracketing_grid_points::Array{S,2}) where {T <: AbstractFloat, S <: Integer, N}
+
+    data = zeros(size(bracketing_grid_points,1))
+    for i = 1:length(data)
+      data[i] = y[CartesianIndex(tuple(bracketing_grid_points[i,:]...))]
+    end
+
+    return data
+
+end
+
+function piecewise_linear_evaluate(y::AbstractArray{T,N},x::Union{NTuple{N,Array{T,1}},Array{Array{T,1},1}},point::Union{T,Array{T,1}}) where {T <: AbstractFloat, N}
+
+  b = bracket_nodes(x,point)
+  w = piecewise_linear_weights(x,point)
+
+  d = size(b,2)
+
+  relevant_points = select_bracketing_nodes(b)
+  data = select_relevant_data(y,relevant_points)
+
+  for j = d:-1:1
+
+    new_data = zeros(Int(length(data)/2))
+    for i = 1:length(new_data)
+      new_data[i] = data[2*(i-1)+1] + w[j]*(data[2*i]-data[2*(i-1)+1])
+    end
+
+    data = copy(new_data)
+
+  end
+
+   return data[1]
+
+end
+
+#=
+
 function locate_point_below(x::Array{T,1},point::T) where {T <: AbstractFloat}
 
     if point <= x[1]
@@ -44,6 +138,8 @@ function piecewise_linear_evaluate(y::Array{T,N},x::Union{NTuple{N,Array{T,1}},A
     end
 
 end
+
+=#
 
 function piecewise_linear_evaluate(y::Array{T,1},nodes::Array{T,1},point::T) where {T <: AbstractFloat}
 
